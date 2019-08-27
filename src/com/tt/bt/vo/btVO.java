3821b250d1166422cbb0e2ac74b3e4ec504f6178
LimitL <ghdcjscjs13@gmail.com>
2019-08-27 14:56:40 +0900

    상욱 코딩

---
+++ src/com/tt/bt/vo/btVO.java
@@ -0,0 +1,93 @@
+package com.tt.bt.vo;
+
+public class btVO {
+	
+	
+		private String btno ="";
+		private String mbno ="";
+		private String btwriter ="";
+		private String dname = "";
+		private String gname ="";
+		private String btsubject ="";
+		private String btmakedate ="";
+		private String btstate = "";
+		private String btpath ="";
+		private String btinsertdate ="";
+		private String btupdatedate ="";
+		private String btdeleteyn ="";
+		
+		
+		public String getBtno() {
+			return btno;
+		}
+		public void setBtno(String btno) {
+			this.btno = btno;
+		}
+		public String getMbno() {
+			return mbno;
+		}
+		public void setMbno(String mbno) {
+			this.mbno = mbno;
+		}
+		public String getBtwriter() {
+			return btwriter;
+		}
+		public void setBtwriter(String btwriter) {
+			this.btwriter = btwriter;
+		}
+		public String getDname() {
+			return dname;
+		}
+		public void setDname(String dname) {
+			this.dname = dname;
+		}
+		public String getGname() {
+			return gname;
+		}
+		public void setGname(String gname) {
+			this.gname = gname;
+		}
+		public String getBtsubject() {
+			return btsubject;
+		}
+		public void setBtsubject(String btsubject) {
+			this.btsubject = btsubject;
+		}
+		public String getBtmakedate() {
+			return btmakedate;
+		}
+		public void setBtmakedate(String btmakedate) {
+			this.btmakedate = btmakedate;
+		}
+		public String getBtstate() {
+			return btstate;
+		}
+		public void setBtstate(String btstate) {
+			this.btstate = btstate;
+		}
+		public String getBtpath() {
+			return btpath;
+		}
+		public void setBtpath(String btpath) {
+			this.btpath = btpath;
+		}
+		public String getBtinsertdate() {
+			return btinsertdate;
+		}
+		public void setBtinsertdate(String btinsertdate) {
+			this.btinsertdate = btinsertdate;
+		}
+		public String getBtupdatedate() {
+			return btupdatedate;
+		}
+		public void setBtupdatedate(String btupdatedate) {
+			this.btupdatedate = btupdatedate;
+		}
+		public String getBtdeleteyn() {
+			return btdeleteyn;
+		}
+		public void setBtdeleteyn(String btdeleteyn) {
+			this.btdeleteyn = btdeleteyn;
+		}
+	
+}

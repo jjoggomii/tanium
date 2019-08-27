1902106d8ecbb99e1bba8d1ed00bce25bcd5fef0
Kkokko- <m.sun.1290@gmail.com>
2019-08-27 13:58:59 +0900

    명 테스추

--- src/com/tt/vr/controller/VrController.java
+++ src/com/tt/vr/controller/VrController.java
@@ -15,6 +15,7 @@ import com.tt.vr.vo.VrVO;
 public class VrController {
 	
 	private static final String CONTEXT_PATH = "vr";
+	private static final String CONTEXT_PATH2 = "vr2";
 	
 	@Autowired
 	private VrServiceDAO vrService;

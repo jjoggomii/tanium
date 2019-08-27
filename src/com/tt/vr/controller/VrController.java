b831b13f53ac893cf87e40c76081cc7bca66db8f
Kkokko- <m.sun.1290@gmail.com>
2019-08-27 14:09:18 +0900

    Revert "ㅡ,ㅡ,ㅡ,"
    This reverts commit 25aad018bbb87d3de6cf64a11cf2137134eedda2.

--- src/com/tt/vr/controller/VrController.java
+++ src/com/tt/vr/controller/VrController.java
@@ -15,6 +15,7 @@ import com.tt.vr.vo.VrVO;
 public class VrController {
 	
 	private static final String CONTEXT_PATH = "vr";
+	private static final String CONTEXT_PATH2 = "YangYangTest";
 	
 	@Autowired
 	private VrServiceDAO vrService;
